72027253e1569b6ae38a4ea91f63db63b676b13a
BradleyWeibel-DCU <bradley.weibel2@mail.dcu.ie>
2024-04-08 02:44:53 +0200

    1st commit. Stable, empty version. TODO: everything

---
+++ app/src/main/java/com/example/sda_a5_2024_bradleyweibel/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sda_a5_2024_bradleyweibel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
